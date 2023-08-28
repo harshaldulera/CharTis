720edf9d014260f95eaab29657786810d90bcbc2
Harshal Dulera <harshaldulera82@gmail.com>
2023-08-29 04:14:07 +0530

    try: change maps

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,68 @@
-import React, { useEffect } from "react";
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import mapboxgl from "mapbox-gl";
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+import React, { useEffect, useRef, useState } from "react";
+import '../styles/Home.css';
+import "mapbox-gl/dist/mapbox-gl.css";
+
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_KEY;
 
 function Home() {
-  useEffect(() => {
-    // Create a map centered at a specific location (e.g., Ghatkopar)
-    const map = L.map('map').setView([19.0896, 72.9806], 12);
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(72.87);
+    const [lat, setLat] = useState(19.07);
+    const [zoom, setZoom] = useState(9);
 
-    // Add a tile layer to the map (using OpenStreetMap tiles)
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    useEffect(() => {
+        // Create a new map when the component mounts
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: "mapbox://styles/mapbox/streets-v12",
+            center: [lng, lat],
+            zoom: zoom,
+        });
 
-    // Add a marker at the center of the map
-    L.marker([19.0896, 72.9806]).addTo(map)
-      .bindPopup('Ghatkopar')
-      .openPopup();
-  }, []);
+        // Add controls and directions here as you did before
+        map.current.addControl(new mapboxgl.NavigationControl());
+        map.current.addControl(new mapboxgl.FullscreenControl());
+        map.current.addControl(
+            new mapboxgl.GeolocateControl({
+                positionOptions: {
+                    enableHighAccuracy: true,
+                },
+                trackUserLocation: true,
+            })
+        );
+        map.current.addControl(
+            new MapboxDirections({
+                accessToken: mapboxgl.accessToken,
+            }),
+            "top-left"
+        );
 
-  return (
-    <div>
-      <h2>Google Maps Clone using Leaflet</h2>
-      <div id="map" style={{ width: '100%', height: '500px' }}></div>
-    </div>
-  );
+        // Event listener for map move
+        map.current.on("move", () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        });
+
+        // Cleanup when the component unmounts
+        return () => {
+            if (map.current) {
+                map.current.remove();
+            }
+        };
+    }, [lng, lat, zoom]);
+
+    return (
+        <div className="App">
+            <div className="sidebar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div ref={mapContainer} className="map-container" />
+        </div>
+    );
 }
 
 export default Home;

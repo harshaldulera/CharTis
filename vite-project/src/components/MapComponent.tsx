32ebe3866f6cc359510cc68ddbaf134ec5a604a4
Harshal Dulera <harshaldulera82@gmail.com>
2023-09-01 20:35:48 +0530

    feat: map fixed

--- vite-project/src/components/MapComponent.tsx
+++ vite-project/src/components/MapComponent.tsx
@@ -43,9 +43,18 @@ const MapComponent: React.FC = () => {
     };
   }, [map, lat, lng, zoom]);
 
+  const mapStyle = {
+    width: '100vw',
+    height: '100vh',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    zIndex: 0,
+  };
+
   return (
-    <div style={{ width: '100%', height: '100%' }}>
-      <div ref={mapContainer} className="map-container" style={{ width: "100vw", height: "100vh" }} />
+    <div style={mapStyle}>
+      <div ref={mapContainer} className="map-container" style={mapStyle} />
     </div>
   );
 };
